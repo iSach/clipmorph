427b3ca25cb692caa4a8467fa142e658ee8ff87f
Sacha Lewin <sacha.lewin@me.com>
2024-04-21 16:02:05 +0200

    Fixed code style issues

--- clipmorph/nn/__init__.py
+++ clipmorph/nn/__init__.py
@@ -1,2 +1,4 @@
 from clipmorph.nn.backbone import Vgg19
 from clipmorph.nn.unet import FastStyleNet
+
+__all__ = ['Vgg19', 'FastStyleNet']
--- tests/nn/test_unet.py
+++ tests/nn/test_unet.py
@@ -9,6 +9,6 @@ def test_faststylenet():
     assert isinstance(net, torch.nn.Module)
 
     x = torch.randn(1, 3, 256, 256)
-    assert x.requires_grad == False
+    assert not x.requires_grad
     y = net(x)
     assert y.size() == x.size()
