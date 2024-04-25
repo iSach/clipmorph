af69c58de9a7134f702cb81ed1b4ffdadf57302d
Sacha Lewin <sacha.lewin@me.com>
2024-04-25 16:40:08 +0200

    Turned Debug mode on again

--- app/app.py
+++ app/app.py
@@ -74,4 +74,4 @@ def upload_file():
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=False, host='0.0.0.0', port=8080, allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=8080, allow_unsafe_werkzeug=True)
